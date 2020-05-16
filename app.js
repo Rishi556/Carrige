9c0282ec97d34b21d929a1d744d3f4a6e6dd3f63
Rishi Panthee <rishipanthee@hotmail.com>
2020-05-15 21:32:21 -0500

    cors + increase size

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 let stream_blocks = require("./src/stream_blocks.js")
 let express = require('express')
 let bodyParser = require('body-parser')
+let cors = require("cors")
 
 stream_blocks.startStreaming()
 
 let app = express()
+
+app.use(cors())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
